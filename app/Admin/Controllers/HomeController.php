129d182d831427ab9197c3e9c6556b8462ff5c8e
chenkunbest <631282467@qq.com>
2020-06-02 17:33:29 +0800

    20200602

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
                     $column->row(Card::make('今日总销售', \App\Models\CodeReceivingRecord::where('created_at', '>', Carbon::today())->where('status', '1')->sum('amount')));
                 });
                 $row->column(4, function (Column $column) {
-                    $column->row(Card::make('今日接码总数', \App\Models\CodeReceivingRecord::where('created_at', '>', Carbon::today())->count()));
+                    $column->row(Card::make('今日接码总数', \App\Models\CodeReceivingRecord::where('created_at', '>', Carbon::today())->where('status', '1')->count()));
                 });
 
 
